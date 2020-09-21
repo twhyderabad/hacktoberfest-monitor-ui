c7ea3b16dfe01b7170233f3665afb627ed1bc579
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2020-09-21 17:46:32 +0530

    Initial commit. Add only optimized build

---
+++ precache-manifest.9b4580daaa0a5c21205b24265d02a434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b50a0223e305c08a1e2143528bff48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b1b78684bcfaa6def0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d5edd7a3ee0537c92db2",
+    "url": "/static/js/2.91a537a2.chunk.js"
+  },
+  {
+    "revision": "92b1b78684bcfaa6def0",
+    "url": "/static/js/main.f748ab52.chunk.js"
+  },
+  {
+    "revision": "500c6a4d9d125d185445",
+    "url": "/static/js/runtime-main.4ea2b256.js"
+  }
+]);
